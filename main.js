cafcfd42374291101204f09c599cb9d916890fff
Hamza <hamzadanden2@gmail.com>
2019-11-05 10:02:24 +0100

    my first version

--- main.js
+++ main.js
@@ -1,24 +1,48 @@
-var ItemsArray = [];
+totalPrice = 0;
 
-
-
-var x1 = 0;
-var p1 = 0;
-$('#item1plus').on('click',function(){
-	$("#it1").text(x1 = x1 + 1);
-	//ItemsArray.push($('#title1').text())
-    //x1 = x1 + 1;
-    y = x1.toString()
-    console.log(y)
-	$('ul').append($('<li>'+ $("#title1").text()+'</li>') + x1)
-
-})
-
-$('#item1minus').on('click',function(){
-	if (x1 > 0){
-	$("#it1").text(x1 = x1 - 1);	
+object1 = {
+	name: "Margherita Pizza",
+	price: 9.99,
+	orderTime: 0
 }
 
+$('#item1plus').on('click',function(){
+	totalPrice += object1.price;
+	object1.orderTime += 1;
+	if (object1.orderTime > 1){
+		
+		$("#it1").text(object1.orderTime);
+		$('#li1').remove();
+		$('ul').append('<li id="li1">' + object1.name + ' x' + object1.orderTime + '</li>');
+
+	} else {
+		$("#it1").text(object1.orderTime);
+		$('ul').append('<li id="li1">' + object1.name + ' x' + object1.orderTime + '</li>');
+	}
+	console.log(totalPrice)
+	
+
 })
+
+
+$('#item1minus').on('click',function(){
+	totalPrice -= object1.price;
+	if(object1.orderTime > 0) {
+		if(object1.orderTime - 1 === 0){
+			object1.orderTime -= 1;
+			$('#li1').remove();
+			$("#it1").text(object1.orderTime);
+		} else {
+			object1.orderTime -= 1;
+			$("#it1").text(object1.orderTime);
+			$('#li1').remove();
+			$('ul').append('<li id="li1">' + object1.name + ' x' + object1.orderTime + '</li>');
+		}
+	}
+
+
+	console.log(totalPrice);
+})
+
 
 
